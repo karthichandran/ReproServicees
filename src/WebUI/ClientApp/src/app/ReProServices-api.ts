db3e0ff1959bb715dff73b02e221d219deedb1b8
karthi <karthi@leansys.in>
2020-03-16 18:41:58 +0530

    updated Sellers

--- src/WebUI/ClientApp/src/app/ReProServices-api.ts
+++ src/WebUI/ClientApp/src/app/ReProServices-api.ts
@@ -16,7 +16,9 @@ export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL');
 
 export interface ISellerClient {
     get(): Observable<SellerDto[]>;
+    create(command: CreateSellerCommand): Observable<number>;
     getById(id: number): Observable<SellerDto>;
+    update(id: number, command: UpdateSellerCommand): Observable<FileResponse>;
 }
 
 @Injectable({
@@ -84,6 +86,58 @@ export class SellerClient implements ISellerClient {
         return _observableOf<SellerDto[]>(<any>null);
     }
 
+    create(command: CreateSellerCommand): Observable<number> {
+        let url_ = this.baseUrl + "/api/Seller";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(command);
+
+        let options_ : any = {
+            body: content_,
+            observe: "response",
+            responseType: "blob",			
+            headers: new HttpHeaders({
+                "Content-Type": "application/json", 
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request("post", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processCreate(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processCreate(<any>response_);
+                } catch (e) {
+                    return <Observable<number>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<number>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processCreate(response: HttpResponseBase): Observable<number> {
+        const status = response.status;
+        const responseBlob = 
+            response instanceof HttpResponse ? response.body : 
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }};
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = resultData200 !== undefined ? resultData200 : <any>null;
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<number>(<any>null);
+    }
+
     getById(id: number): Observable<SellerDto> {
         let url_ = this.baseUrl + "/api/Seller/{id}";
         if (id === undefined || id === null)
@@ -133,6 +187,59 @@ export class SellerClient implements ISellerClient {
             }));
         }
         return _observableOf<SellerDto>(<any>null);
+    }
+
+    update(id: number, command: UpdateSellerCommand): Observable<FileResponse> {
+        let url_ = this.baseUrl + "/api/Seller/{id}";
+        if (id === undefined || id === null)
+            throw new Error("The parameter 'id' must be defined.");
+        url_ = url_.replace("{id}", encodeURIComponent("" + id)); 
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(command);
+
+        let options_ : any = {
+            body: content_,
+            observe: "response",
+            responseType: "blob",			
+            headers: new HttpHeaders({
+                "Content-Type": "application/json", 
+                "Accept": "application/octet-stream"
+            })
+        };
+
+        return this.http.request("put", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processUpdate(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processUpdate(<any>response_);
+                } catch (e) {
+                    return <Observable<FileResponse>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<FileResponse>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processUpdate(response: HttpResponseBase): Observable<FileResponse> {
+        const status = response.status;
+        const responseBlob = 
+            response instanceof HttpResponse ? response.body : 
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }};
+        if (status === 200 || status === 206) {
+            const contentDisposition = response.headers ? response.headers.get("content-disposition") : undefined;
+            const fileNameMatch = contentDisposition ? /filename="?([^"]*?)"?(;|$)/g.exec(contentDisposition) : undefined;
+            const fileName = fileNameMatch && fileNameMatch.length > 1 ? fileNameMatch[1] : undefined;
+            return _observableOf({ fileName: fileName, data: <any>responseBlob, status: status, headers: _headers });
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<FileResponse>(<any>null);
     }
 }
 
@@ -782,6 +889,78 @@ export interface ISellerDto {
     pan?: string | undefined;
     mobileNo?: string | undefined;
     stateID?: number;
+}
+
+export class CreateSellerCommand implements ICreateSellerCommand {
+    sellerDto?: SellerDto | undefined;
+
+    constructor(data?: ICreateSellerCommand) {
+        if (data) {
+            for (var property in data) {
+                if (data.hasOwnProperty(property))
+                    (<any>this)[property] = (<any>data)[property];
+            }
+        }
+    }
+
+    init(_data?: any) {
+        if (_data) {
+            this.sellerDto = _data["sellerDto"] ? SellerDto.fromJS(_data["sellerDto"]) : <any>undefined;
+        }
+    }
+
+    static fromJS(data: any): CreateSellerCommand {
+        data = typeof data === 'object' ? data : {};
+        let result = new CreateSellerCommand();
+        result.init(data);
+        return result;
+    }
+
+    toJSON(data?: any) {
+        data = typeof data === 'object' ? data : {};
+        data["sellerDto"] = this.sellerDto ? this.sellerDto.toJSON() : <any>undefined;
+        return data; 
+    }
+}
+
+export interface ICreateSellerCommand {
+    sellerDto?: SellerDto | undefined;
+}
+
+export class UpdateSellerCommand implements IUpdateSellerCommand {
+    sellerDto?: SellerDto | undefined;
+
+    constructor(data?: IUpdateSellerCommand) {
+        if (data) {
+            for (var property in data) {
+                if (data.hasOwnProperty(property))
+                    (<any>this)[property] = (<any>data)[property];
+            }
+        }
+    }
+
+    init(_data?: any) {
+        if (_data) {
+            this.sellerDto = _data["sellerDto"] ? SellerDto.fromJS(_data["sellerDto"]) : <any>undefined;
+        }
+    }
+
+    static fromJS(data: any): UpdateSellerCommand {
+        data = typeof data === 'object' ? data : {};
+        let result = new UpdateSellerCommand();
+        result.init(data);
+        return result;
+    }
+
+    toJSON(data?: any) {
+        data = typeof data === 'object' ? data : {};
+        data["sellerDto"] = this.sellerDto ? this.sellerDto.toJSON() : <any>undefined;
+        return data; 
+    }
+}
+
+export interface IUpdateSellerCommand {
+    sellerDto?: SellerDto | undefined;
 }
 
 export class CreateTodoItemCommand implements ICreateTodoItemCommand {
